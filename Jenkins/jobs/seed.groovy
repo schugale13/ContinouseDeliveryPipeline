001550f7613dbd7e3fc793bb0cba1924fff89551
aschug <alexander.schug@hotmail.com>
2018-08-24 10:07:59 +0200

    adding seed job
    fixing problems with seed job

--- Jenkins/jobs/seed.groovy
+++ Jenkins/jobs/seed.groovy
@@ -1,20 +1,29 @@
-import hudson.model.*
-import jenkins.model.*;
+import hudson.triggers.TimerTrigger;
 import javaposse.jobdsl.plugin.*;
+import jenkins.model.Jenkins;
+import hudson.model.FreeStyleProject
 
-def jenkins = Jenkins.getInstance();
+jenkins = Jenkins.instance;
 
-def job = new FreeStyleProject(jenkins, "Seed-Jobs");
-job.setAssignedLabel(null);
+jobName = "PSE JOBS seeder";
+timerTrigger = new TimerTrigger("H/15 * * * *");
+dslBuilder = new ExecuteDslScripts(scriptText: '''
+job('PSE-GitCheock-outifchanges') {
+    scm {
+        git('https://github.com/schugale13/PSE-Project2017.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+}
+''');
+dslProject = new FreeStyleProject(jenkins, jobName);
+dslProject.addTrigger(timerTrigger);
+dslProject.createTransientActions();
+dslProject.getPublishersList().add(dslBuilder);
+jenkins.add(dslProject, jobName);
+timerTrigger.start(dslProject, true);
 
-job.setCustomWorkspace("/usr/share/jenkins/ref/init.groovy.d/job-dsl");
-def ExecuteDslScripts.ScriptLocation scriptlocationFileSys = new ExecuteDslScripts.ScriptLocation('false', "*.groovy", null);
-def ExecuteDslScripts executeDslScripts = new ExecuteDslScripts(scriptlocationFileSys, false, RemovedJobAction.IGNORE);
 
-job.buildersList.add(executeDslScripts);
 
-jenkins.putItem(job);
-jenkins.reload();
 
-def jobRef = jenkins.getItem(job.getName());
-jenkins.getQueue().schedule(jobRef, 10);
--- Jenkins/user/security.groovy
+++ Jenkins/user/security.groovy
@@ -3,6 +3,9 @@
 import jenkins.model.*
 import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
+import javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration
+import jenkins.model.GlobalConfiguration
+
  
 def instance = Jenkins.getInstance()
  
@@ -15,3 +18,7 @@ instance.setAuthorizationStrategy(strategy)
 instance.save()
  
 Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
+
+// disable Job DSL script approval
+GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).useScriptSecurity=false
+GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).save()
