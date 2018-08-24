0bc1209fc121d9f454c2a78a39d925b143514d2b
schugale13 <alexander.schug@edu.fh-joanneum.at>
2018-08-24 11:42:54 +0200

    add first seedet maven job
    testing maven job

--- Jenkins/jobs/PSECheckoutGIT.groovy
+++ Jenkins/jobs/PSECheckoutGIT.groovy
@@ -1,1 +1,18 @@
+String basePath = 'PSEProject'
+String repo = 'https://github.com/schugale13/PSE-Project2017.git'
 
+folder(basePath) {
+    description 'This Jobs are Running all PSE Project Jobs'
+}
+
+job("$basePath/Maven-PSE-build") {
+    scm {
+        github repo
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+    steps {
+        maven('clean package')
+    }
+}
