07d109fcc2af87aa4e027485d5e7b13484e8e4fd
aschug <alexander.schug@hotmail.com>
2018-08-30 15:24:32 +0200

    try the pipeline again
    do it

--- Jenkins/jobs/PSEJobs.groovy
+++ Jenkins/jobs/PSEJobs.groovy
@@ -132,11 +132,37 @@ job("$basePath/05_Maven-PSE-build-Release for Live System") {
         }
 
 
-
-
     }
 }
 
-job('example-1') {
-    deliveryPipelineConfiguration('01_Maven-PSE-Documentation"')
+pipelineJob('PipelineJobtest') {
+    definition {
+        cps {
+            sandbox()
+            script("""
+node {
+    def mvnHome
+    stage('Preparation') {
+        github repo
+        mvnHome = tool 'M3'
+    }
+    stage('Build') {
+        if (isUnix()) {
+            sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+        } else {
+            bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+        }
+    }
+    stage('Results') {
+        junit '**/target/surefire-reports/TEST-*.xml'
+        archive 'target/*.jar'
+    }
+    stage('Publish') {
+        nexusPublisher nexusInstanceId: 'localNexus', nexusRepositoryId: 'releases', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'war/target/jenkins.war']], mavenCoordinate: [artifactId: 'jenkins-war', groupId: 'org.jenkins-ci.main', packaging: 'war', version: '2.23']]]
+    }
 }
+      """.stripIndent())
+        }
+    }
+}
+
